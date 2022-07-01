a73f2f8e7d4701798d64b8d2186f35726e3e342a
Michael Jungmair <jungmair@in.tum.de>
2022-07-01 15:46:41 +0200

    Fix wrong paths for loc computation

--- scripts/codestats/codestats-lingodb-exec.sh
+++ scripts/codestats/codestats-lingodb-exec.sh
@@ -3,8 +3,8 @@
 IR_INCL_DIRS="lingodb/include/mlir/Dialect/RelAlg lingodb/lib/RelAlg lingodb/include/mlir/Dialect/DB lingodb/include/mlir/Dialect/DSA lingodb/lib/DB lingodb/lib/DB/Transforms lingodb/lib/DSA lingodb/include/mlir/Dialect/util lingodb/lib/util"
 IR_EXCL_DIRS="Transforms"
 
-OP_INCL_DIRS="lingodb/include/mlir/Conversion/RelAlgToDB"
-LOW_INCL_DIRS="lingodb/include/mlir/Conversion/DBToArrowStd lingodb/lib/Conversion/DBToArrowStd lingodb/include/mlir/Conversion/DSAToStd lingodb/lib/Conversion/DSAToStd lingodb/include/mlir/Conversion/UtilToLLVM lingodb/lib/Conversion/UtilToLLVM lingodb/include/runner lingodb/lib/runner"
+OP_INCL_DIRS="lingodb/include/mlir/Conversion/RelAlgToDB lingodb/lib/Conversion/RelAlgToDB"
+LOW_INCL_DIRS="lingodb/include/mlir/Conversion/DBToStd lingodb/lib/Conversion/DBToStd lingodb/include/mlir/Conversion/DSAToStd lingodb/lib/Conversion/DSAToStd lingodb/include/mlir/Conversion/UtilToLLVM lingodb/lib/Conversion/UtilToLLVM lingodb/include/runner lingodb/lib/runner"
 RT_INCL_DIRS="lingodb/include/runtime lingodb/lib/runtime"
 IR_VAL=$(docker run -v $PWD:/data cloc-docker --exclude-lang=CMake --json --force-lang-def=scripts/codestats/cloc.defs --exclude-dir=$IR_EXCL_DIRS $IR_INCL_DIRS  | jq '[."C++".code, ."C/C++ Header".code, ."TableGen".code] | add')
 OP_VAL=$(docker run -v $PWD:/data cloc-docker --exclude-lang=CMake --json --force-lang-def=scripts/codestats/cloc.defs  $OP_INCL_DIRS  | jq '[."C++".code, ."C/C++ Header".code, ."TableGen".code] | add')
